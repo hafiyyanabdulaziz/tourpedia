663a3082aa2ad55b0bb32ab95514d10204df4f29
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-06-03 13:43:21 +0700

    slicing culinary selesai

--- lib/ui/pages/culinary.dart
+++ lib/ui/pages/culinary.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tourpedia/ui/widgets/card_explore.dart';
+import 'package:tourpedia/ui/widgets/card_must_see.dart';
+import 'package:tourpedia/ui/widgets/custom_header.dart';
 
 class Culinary extends StatefulWidget {
   const Culinary({Key? key}) : super(key: key);
@@ -11,11 +14,53 @@ class _CulinaryState extends State<Culinary> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Culinary'),
-      ),
-      body: const Center(
-        child: Text('data'),
+      body: ListView(
+        children: [
+          CustomHeader(
+            searchTap: () {},
+            type: Type.culinary,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 10),
+            child: Text(
+              'Must See',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          // ignore: sized_box_for_whitespace
+          Container(
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const CardMustSee(),
+                const CardMustSee(),
+                const CardMustSee(),
+                const CardMustSee(),
+                const CardMustSee(),
+                const CardMustSee(),
+                const CardMustSee(),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Text(
+              'Explore',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const CardExplore(),
+          const CardExplore(),
+          const CardExplore(),
+        ],
       ),
     );
   }
