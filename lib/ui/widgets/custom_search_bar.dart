3146023fea36df5b6be1a135fa42049f6ee36f21
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-06-29 07:43:52 +0700

    membuat kotak search baru

--- lib/ui/widgets/custom_search_bar.dart
+++ lib/ui/widgets/custom_search_bar.dart
@@ -1,43 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:touchable_opacity/touchable_opacity.dart';
 import 'package:tourpedia/utils/my_colors.dart';
 
-class CustomSearchBar extends StatelessWidget {
+class CustomSearchBar extends StatefulWidget {
   final GestureTapCallback? searchTap;
   const CustomSearchBar({Key? key, required this.searchTap}) : super(key: key);
 
   @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  TextEditingController textEditingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return TouchableOpacity(
-      onTap: searchTap,
-      child: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width - 40,
-          padding:
-              const EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 5),
-          margin: const EdgeInsets.only(top: 20),
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
+    return Center(
+      child: Container(
+        decoration: const BoxDecoration(
             color: MyColors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        margin: const EdgeInsets.only(top: 20, bottom: 10),
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        width: MediaQuery.of(context).size.width - 40,
+        child: TextField(
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Search',
+            icon: Icon(Icons.search),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              Text(
-                'Search',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.4),
-                  fontSize: 15,
-                ),
-              ),
-              Icon(
-                Icons.search,
-                color: Colors.black.withOpacity(0.4),
-              ),
-            ],
-          ),
+          controller: textEditingController,
         ),
       ),
     );
