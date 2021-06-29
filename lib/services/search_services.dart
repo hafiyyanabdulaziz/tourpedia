e4f02f9687a489c856776d987d57bc559d601717
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-06-29 08:43:33 +0700

    sudah membuat page search destination

--- lib/services/search_services.dart
+++ lib/services/search_services.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tourpedia/models/search_culinary_model.dart';
+import 'package:tourpedia/models/search_tourism_model.dart';
 import 'package:tourpedia/utils/settings.dart';
 import 'package:http/http.dart' as http;
 
@@ -14,6 +15,26 @@ class SearchServices {
         final culinariesExploreModel =
             searchCulinaryModelFromJson(response.body);
         return culinariesExploreModel;
+      } else {
+        debugPrint("error status " + response.statusCode.toString());
+        return null;
+      }
+    } catch (e) {
+      debugPrint("error catch $e");
+      return null;
+    }
+  }
+
+  Future<SearchTourismModel?> getDataDestinationsSearch(String text) async {
+    String url = Settings.urlBackend + '/api/destinations/?search=' + text;
+    try {
+      http.Response response = await http
+          .get(Uri.parse(url), headers: {"Accept": "aplication/json"});
+      if (response.statusCode == 200) {
+        debugPrint("data culinary search success");
+        final destinationsSearchModel =
+            searchTourismModelFromJson(response.body);
+        return destinationsSearchModel;
       } else {
         debugPrint("error status " + response.statusCode.toString());
         return null;
